070da8d0a281cc17fe9359be7bed29bc06e7ad5b
Bournane Abdelkrim <abdelkrim.bournane@gmail.com>
2023-03-08 19:02:04 +0100

    expose necessary core types.

--- src/core/lib/core.dart
+++ src/core/lib/core.dart
@@ -1,1 +1,14 @@
 library core;
+
+export './src/blocs/budget_bloc.dart'
+    show
+        BudgetManagerBloc,
+        UninitializedBudget,
+        InitializingBudget,
+        DetailedBudget,
+        SetupBudgetDetails,
+        AddPeriodIncome,
+        AddPeriodExpense;
+export 'src/entities/budget_details.dart' show BudgetDetails;
+export 'src/entities/period_expense.dart' show PeriodExpense;
+export 'src/entities/period_income.dart' show PeriodIncome;
