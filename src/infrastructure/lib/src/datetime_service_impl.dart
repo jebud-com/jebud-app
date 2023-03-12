41a383d35896a2e47f4c32ebd826ddc8052f844c
Bournane Abdelkrim <abdelkrim.bournane@gmail.com>
2023-03-12 15:11:42 +0100

    implement date time service.

---
+++ src/infrastructure/lib/src/datetime_service_impl.dart
@@ -0,0 +1,13 @@
+﻿
+
+
+import 'package:core/core.dart';
+
+class DateTimeServiceImpl extends DateTimeService {
+  @override
+  DateTime get startOfCurrentMonth => DateTime.now().copyWith(day: 1); 
+
+  @override
+  DateTime get today => DateTime.now(); 
+  
+}
---
+++ src/infrastructure/test/date_time_impl_test.dart
@@ -0,0 +1,33 @@
+﻿
+
+
+import 'package:core/core.dart';
+import 'package:infrastructure/src/datetime_service_impl.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  
+  test("date time service return first day of current month", () {
+    DateTimeService dateTimeService = DateTimeServiceImpl(); 
+    
+    var result = dateTimeService.startOfCurrentMonth;
+    
+    expect(result, isNotNull);
+    expect(result.day, equals(1));
+    expect(result.month, equals(DateTime.now().month));
+    expect(result.year, equals(DateTime.now().year));
+  });
+  
+  test("date time service return current day of current month", () {
+    DateTimeService dateTimeService = DateTimeServiceImpl();
+    
+    var result = dateTimeService.today;
+    
+    expect(result, isNotNull);
+    expect(result.day, equals(DateTime.now().day));
+    expect(result.month, equals(DateTime.now().month));
+    expect(result.year, equals(DateTime.now().year));
+  });
+  
+}
