af6d9e5e6140f68045b9d5f8e815b132fb2c492f
Bournane Abdelkrim <abdelkrim.bournane@gmail.com>
2023-03-12 02:33:54 +0100

    expose Failure and CannotFindBudgetDetails from core.

--- src/core/lib/core.dart
+++ src/core/lib/core.dart
@@ -16,3 +16,6 @@ export 'src/entities/budget_details.dart' show BudgetDetails;
 export 'src/entities/period_expense.dart' show PeriodExpense;
 export 'src/entities/period_income.dart' show PeriodIncome;
 export 'src/repository/budget_repository.dart' show BudgetRepository;
+
+export 'src/failures/failure.dart' show Failure;
+export 'src/failures/cannot_find_budget_details.dart' show CannotFindBudgetDetails;
